f293867eb876f91bc04c49141ccea101d652221c
akshitgaur2005 <akshitgaur@proton.me>
2024-05-07 20:21:25 +0530

    Second Commit

--- main.py
+++ main.py
@@ -2,31 +2,44 @@ import gymnasium as gym
 import random
 from collections import deque, namedtuple
 import numpy as np
+import torch
+from torch import nn, optim
 
 def main():
-    env = gym.make("LunarLander-v2", render_mode="human")
+    env = gym.make("LunarLander-v2", render_mode="rgb_array")
 
     state, info = env.reset()
 
-    num_episodes = 10
-    agent = DQN()
+    num_episodes = 100
+    n_obs = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+    
+    agent = DQN(n_obs, n_actions)
 
     event = namedtuple("Event", "state, action, reward, next_state")
 
     for i in range(num_episodes):
-        action = agent.get_action(state)
-        print(action)
-        next_state, reward, terminated, truncated, info = env.step(action)
-        agent.memory.append(event(state, action, reward, next_state))
-        next_state = state
+        print(i)
+        term, trunc = False, False
+        while not (term or trunc):
+            action = agent.get_action(state)
+            #print(action)
+            next_state, reward, term, trunc, info = env.step(action)
+            agent.memory.append(event(state, action, reward, next_state))
+            state = next_state
 
     states, actions, rewards, next_states = list(zip(*agent.memory))
-    print(np.array(states))
+    print(np.array(next_states).shape)
 
 class DQN():
 
-    def __init__(self):
+    def __init__(self, n_obs, n_actions):
         self.memory = deque()
+        self.model = nn.Sequential(
+                nn.LazyLinear(64), nn.ReLU(),
+                nn.LazyLinear(16), nn.ReLU(),
+                nn.LazyLinear(n_actions), nn.ReLU(),
+                )
 
     def get_action(self, state):
         return random.randrange(4)
